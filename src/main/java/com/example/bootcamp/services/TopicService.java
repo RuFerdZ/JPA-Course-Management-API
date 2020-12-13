06f1ee6fb8d0c7e042d117eb57ea03a5308a36ac
Rusiru Fernando <rusiruhfernando@outlook.com>
2020-12-14 04:11:22 +0530

    working with hardcoded data

--- src/main/java/com/example/bootcamp/services/TopicService.java
+++ src/main/java/com/example/bootcamp/services/TopicService.java
@@ -1,36 +1,44 @@
 package com.example.bootcamp.services;
 
 import com.example.bootcamp.models.Topic;
-import com.example.bootcamp.repository.TopicRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-    @Autowired
-    private TopicRepository topicRepository;
+
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("spring", "Spring Framework", "Spring Framework Description"),
+            new Topic("java", "Core Java", "Core Java Description"),
+            new Topic("javascript", "JavaScript", "JavaScript Description")
+    ));
 
     public List<Topic> getAllTopics(){
-        return topicRepository.findAll();
+        return topics;
     }
 
     public Topic getTopic(String id){
-        return topicRepository.findById(id).get();
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
     }
 
     public void addTopic(Topic topic){
-        topicRepository.save(topic);
+        topics.add(topic);
     }
 
     public void updateTopic(String id, Topic topic){
-        topicRepository.save(topic); // it checks whether there is already a pk in the same id
+        for (int index = 0; index<topics.size(); index++){
+            if (topics.get(index).getId().equals(id)){
+                topics.set(index, topic);
+                return;
+            }
+        }
     }
 
     public void deleteTopic(String id) {
-        topicRepository.deleteById(id);
+        topics.removeIf(t -> t.getId().equals(id));
     }
 }
