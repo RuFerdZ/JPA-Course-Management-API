6db4ef10c75845d03ac383d9567436d3826ed75f
Rusiru Fernando <rusiruhfernando@outlook.com>
2020-12-13 06:05:58 +0530

    hardoded getAllTopics endpoint for testing

---
+++ src/main/java/com/example/bootcamp/controllers/TopicController.java
@@ -0,0 +1,21 @@
+package com.example.bootcamp.controllers;
+
+import com.example.bootcamp.models.Topic;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics(){
+        return Arrays.asList(
+                new Topic("spring", "Spring Framework", "Spring Framework Description"),
+                new Topic("java", "Core Java", "Core Java Description"),
+                new Topic("javascript", "JavaScript", "JavaScript Description")
+        );
+    }
+}
